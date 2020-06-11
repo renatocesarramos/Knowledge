e4e6ee027f225c0a93a7cf046a58dfa41602820e
Renato Ramos <renatocesarramos@gmail.com>
2020-06-11 15:59:56 -0300

    Up Files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Knowledge
- Frontend da aplicação desenvolvida em Vue.js
+# frontend
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/config/router.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '@/components/home/Home'
+import AdminPages from '@/components/admin/AdminPages'
+import ArticlesByCategory from '@/components/article/ArticlesByCategory'
+import ArticleById from '@/components/article/ArticleById'
+import Auth from '@/components/auth/Auth'
+
+import { userKey } from '@/global'
+
+Vue.use(VueRouter)
+
+const routes = [{
+  name: 'home',
+  path:'/',
+  component: Home
+} , {
+  name: 'AdminPages',
+  path:'/admin',
+  component: AdminPages,
+  meta: { requiresAdmin: true }
+} , {
+  name: 'ArticlesByCategory',
+  path: '/categories/:id/articles',
+  component: ArticlesByCategory
+}, {
+  name: 'ArticleById',
+  path: '/articles/:id',
+  component: ArticleById
+}, {
+  name: 'auth',
+  path: '/auth',
+  component: Auth
+}]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const json = localStorage.getItem(userKey)
+
+  if(to.matched.some( record => record.meta.requiresAdmin )){
+    const user = JSON.parse(json)
+    user && user.admin ? next() : next( { path: '/'})
+  } else {
+    next();
+  }
+
+})
+
+export default router
